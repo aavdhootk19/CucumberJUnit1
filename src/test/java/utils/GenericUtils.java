cbdf832ba65bd526165659245b3d097287aaec97
avdhoot <avdhoot.kulkarni@worldline.com>
2023-05-11 16:33:53 +0530

    Commit 10

--- src/test/java/utils/GenericUtils.java
+++ src/test/java/utils/GenericUtils.java
@@ -2,42 +2,20 @@ package utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.Iterator;
 import java.util.Properties;
-import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import base.BasePage;
-import pageObjects.OffersPage;
+
 
 public class GenericUtils extends BasePage {
 
-	public static void switchToOfferPage() {
 
-		OffersPage offerPage = new OffersPage();
-		driver.findElement(offerPage.lnkTopDeals).click();
-		String currentWindow = driver.getWindowHandle();
-		Set<String> allWindow = driver.getWindowHandles();
-		Iterator<String> it = allWindow.iterator();
-
-		while (it.hasNext()) {
-			String window = (String) it.next();
-
-			if (!window.equalsIgnoreCase(currentWindow)) {
-				driver.switchTo().window(window);
-			}
-
-		}
-	}
 
 	public static String getDataFromProperty(String key) throws IOException {
 		FileInputStream fis = new FileInputStream(
